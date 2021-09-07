6254f919b690aad4ef9b49c8a7b3ab761b12155c
Joe B <74583686+Fizz14@users.noreply.github.com>
2021-09-07 13:59:54 -0500

    Add files via upload

--- js/current.js
+++ js/current.js
@@ -6,7 +6,7 @@ var m = "current: "
 d = m.concat(d)
 document.getElementById("cur").innerHTML = d;
 
-
+const themeSwitch = document.querySelector('input');
 
 setInterval(function upDate() {
     var d = today.toLocaleDateString("en-US")
@@ -14,3 +14,28 @@ setInterval(function upDate() {
     d = m.concat(d)
     document.getElementById("cur").innerHTML = d;
 }, 3000);
+
+function init() {
+    var mode = window.localStorage.getItem("mode");
+    if(mode == "light") {
+        themeSwitch.checked = true;
+        document.body.classList.remove('darkmode');
+    } else {
+        themeSwitch.checked = false;
+        document.body.classList.add('darkmode');
+    }
+}
+
+themeSwitch.addEventListener('change', () => {
+    
+    
+    var mode = window.localStorage.getItem("mode");
+    if(mode == "dark") {
+        localStorage.setItem("mode", "light");
+        document.body.classList.remove('darkmode');
+    } else {
+        localStorage.setItem("mode", "dark");
+        document.body.classList.add('darkmode');
+    }
+    
+});
